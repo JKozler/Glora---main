92c917eb7c381dd0e787b1f60e2e2ebd88d15f49
Kozler Jakub <kozler.jakub.2017@skola.ssps.cz>
2020-07-01 17:26:38 +0200

    Edit

--- Glora/MainWindow.xaml.cs
+++ Glora/MainWindow.xaml.cs
@@ -30,9 +30,12 @@ namespace Glora
         PromptBuilder pb = new PromptBuilder();
         SpeechRecognitionEngine sre = new SpeechRecognitionEngine();
         Choices choice = new Choices();
+        string search = "";
+
         public MainWindow()
         {
             InitializeComponent();
+            //wind.Height = SystemParameters.PrimaryScreenHeight;
             ss.SpeakAsync("Welcome back.");
             
             //ss.SelectVoiceByHints(VoiceGender.Male);
@@ -40,7 +43,7 @@ namespace Glora
 
         private void speakBtn_Click(object sender, RoutedEventArgs e)
         {
-            choice.Add(new string[] {"hi", "hello", "how are you",  "what is the current time", "open google", "thank you", "close", "cola"});
+            choice.Add(new string[] {"hi", "hello", "how are you",  "what is the current date", "open google", "thank you", "close", "cola"});
             DoubleAnimation doubleAnimation = new DoubleAnimation(15, 100, new Duration(new TimeSpan(0,0,3)));
             ellSpeak.BeginAnimation(WidthProperty, doubleAnimation);
             ellSpeak.BeginAnimation(HeightProperty, doubleAnimation);
@@ -48,52 +51,118 @@ namespace Glora
             try
             {
                 sre.RequestRecognizerUpdate();
-                sre.LoadGrammar(grammar);
+                sre.LoadGrammar(new DictationGrammar());
                 sre.SpeechRecognized += Sre_SpeechRecognized;
                 sre.SetInputToDefaultAudioDevice();
                 sre.RecognizeAsync(RecognizeMode.Multiple);
             }
             catch (Exception ex)
             {
-                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                //MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
 
         private void Sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
         {
-
-            switch (e.Result.Text.ToString())
+            string s = e.Result.Text.ToString();
+            if (s.ToLower().Contains("hello"))
+                ss.SpeakAsync("hello sir");
+            else if (s.ToLower().Contains("current date"))
+                ss.SpeakAsync("current date is " + DateTime.Now.ToLongDateString());
+            else if (s.ToLower().Contains("are you here"))
+                ss.SpeakAsync("I am here sir, dont be afraid");
+            else if (s.ToLower().Contains("browser") || s.ToLower().Contains("web"))
             {
-                case "hello":
-                    ss.SpeakAsync("hello sir");
-                    break;
-
-                case "how are you":
-                    ss.SpeakAsync("really nice, and you?");
-                    break;
-
-                case "what is the current time":
-                    ss.SpeakAsync("current time is " + DateTime.Now.ToLongDateString());
-                    break;
-
-                case "thank you":
-                    ss.SpeakAsync("pleasure is on my side sir");
-                    break;
-
-                case "open google":
-                    ss.SpeakAsync("Okey sir");
-                    Process.Start("chrome", "https://www.google.com");
-                    break;
-
-                case "close":
-                    this.Close();
-                    break;
-
-                default:
-                    ss.SpeakAsync("iam searching for it sir!");
-                    Process.Start("chrome", "https://www.google.com/search?q=" + e.Result.Text.ToString());
-                    break;
+                ss.SpeakAsync("I am opening google sir!");
+                Process.Start("chrome", "https://www.google.com");
             }
+            else if (s.ToLower().Contains("videos"))
+            {
+                ss.SpeakAsync("Opening YT sir");
+                Process.Start("chrome", "https://www.youtube.com");
+            }
+            else if (s.ToLower().Contains("search "))
+            {
+                ss.SpeakAsync("Okay sir, I am searching for it");
+                char[] z = s.ToCharArray();
+                int y = 0;
+                for (int i = 0; i < z.Length; i++)
+                {
+                    if (z[i] == 's' && z[i + 1] == 'e' && z[i + 2] == 'a' && z[i + 3] == 'r' && z[i + 4] == 'c' && z[i + 5] == 'h' && z[i + 6] == ' ')
+                    {
+                        if ((i+7) == z.Length - 1)
+                        {
+                            ss.SpeakAsync("What i have to serach for sir?");
+                        }
+                        else if (z[i + 7] == 'f' && z[i + 8] == 'o' && z[i + 9] == 'r' && z[i + 10] == ' ')
+                        {
+                            y = i + 11;
+                            for (int g = 0; g <= (z.Length - 1 - y); g++)
+                            {
+                                search += z[y];
+                                y++;
+                            }
+                        }
+                        else
+                        {
+                            y = i + 7;
+                            for (int g = 0; g <= (z.Length - 1 - y); g++)
+                            {
+                                search += z[y];
+                                y++;
+                            }
+                        }
+                       
+                    }
+                }
+               
+
+            }
+            if (search != "")
+            {
+                label.Content = search;
+                Process.Start("chrome", "https://www.google.com/search?q=" + search);
+                search = "";
+            }
+            dictateBtn.Content = s;
+            //switch (e.Result.Text.ToString())
+            //{
+            //    case "hello":
+            //        ss.SpeakAsync("hello sir");
+            //        break;
+
+            //    case "how are you":
+            //        ss.SpeakAsync("really nice, and you?");
+            //        break;
+
+            //    case "what is the current date":
+            //        ss.SpeakAsync("current date is " + DateTime.Now.ToLongDateString());
+            //        break;
+
+            //    case "thank you":
+            //        ss.SpeakAsync("pleasure is on my side sir");
+            //        break;
+
+            //    case "open google":
+            //        ss.SpeakAsync("Okey sir");
+            //        Process.Start("chrome", "https://www.google.com");
+            //        break;
+
+            //    case "close":
+            //        this.Close();
+            //        break;
+
+            //    default:
+            //        ss.SpeakAsync("iam searching for it sir!");
+            //        Process.Start("chrome", "https://www.google.com/search?q=" + e.Result.Text.ToString());
+            //        break;
+            //}
+        }
+
+        private void dictateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ss.SpeakAsync("Youcan speak and all what you want will be write in text box above you.");
+
         }
     }
 }
