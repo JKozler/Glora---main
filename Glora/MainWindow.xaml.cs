9244763fa163fca7089b6a1f531f2128b009f2a8
Kozler Jakub <kozler.jakub.2017@skola.ssps.cz>
2020-07-08 19:47:37 +0200

    New features

--- Glora/MainWindow.xaml.cs
+++ Glora/MainWindow.xaml.cs
@@ -39,6 +39,7 @@ namespace Glora
         bool speak = false;
         string name;
         bool anim = true;
+        string[] motivate = { "The harder you work for something, the greater you’ll feel when you achieve it.", "Don’t stop when you’re tired. Stop when you’re done.", "Do something today that your future self will thank you for.", " Sometimes we’re tested not to show our weaknesses, but to discover our strengths.", "The key to success is to focus on goals, not obstacles.", "The pessimist sees difficulty in every opportunity. The optimist sees opportunity in every difficulty.", "It’s not whether you get knocked down, it’s whether you get up.", "If you have something to do today, don't do it and you will have one free day." };
         public MainWindow()
         {
             InitializeComponent();
@@ -104,28 +105,74 @@ namespace Glora
             string s = e.Result.Text.ToString();
             if (s.ToLower().Contains("hello there"))
             {
-                ss.SpeakAsync("General Kenobi!"); 
+                ss.SpeakAsync("General Kenobi!");
                 gloraSay.Items.Add("");
                 gloraSay.Items.Add("General Kenobi!");
             }
             else if (s.ToLower().Contains("hello") || s.ToLower().Contains("hi"))
+            {
                 ss.SpeakAsync("hello sir");
+                gloraSay.Items.Add("");
+                gloraSay.Items.Add("Hello sir!");
+            }
             else if (s.ToLower().Contains("current date"))
+            {
                 ss.SpeakAsync("current date is " + DateTime.Now.ToLongDateString());
+                gloraSay.Items.Add("");
+                gloraSay.Items.Add(DateTime.Now.ToLongDateString());
+            }
             else if (s.ToLower().Contains("time"))
+            {
                 ss.SpeakAsync("current date is " + DateTime.Now.ToLocalTime());
+                gloraSay.Items.Add("");
+                gloraSay.Items.Add(DateTime.Now.ToLocalTime().ToString());
+            }
+            else if (s.ToLower().Contains("10 to 0"))
+            {
+                ss.SpeakAsync("Ok sir!");
+                ss.SpeakAsync("10!"); ss.SpeakAsync("9!"); ss.SpeakAsync("8!"); ss.SpeakAsync("7!"); ss.SpeakAsync("6!"); ss.SpeakAsync("5!"); ss.SpeakAsync("4!"); ss.SpeakAsync("3!"); ss.SpeakAsync("2!"); ss.SpeakAsync("1!"); ss.SpeakAsync("Misík!");
+                gloraSay.Items.Add("");
+                gloraSay.Items.Add("10,9,8....");
+            }
             else if (s.ToLower().Contains("are you here"))
+            {
                 ss.SpeakAsync("I am here sir, dont be afraid");
+                gloraSay.Items.Add("");
+                gloraSay.Items.Add("i am here sir, don't be afraid!");
+            }
             else if (s.ToLower().Contains("fine"))
+            {
                 ss.SpeakAsync("I am glad.");
+                gloraSay.Items.Add("");
+                gloraSay.Items.Add("I am glad!");
+            }
+            else if (s.ToLower().Contains("motivate me"))
+            {
+                ss.SpeakAsync("Lets do it!");
+                Random random = new Random();
+                int y = random.Next(0,7);
+                ss.SpeakAsync(motivate[y]);
+                gloraSay.Items.Add("");
+                gloraSay.Items.Add("Let's do it!");
+            }
             else if (s.ToLower().Contains("how are you"))
+            {
                 ss.SpeakAsync("Thanks for asking sir, i am fine, and you?");
+                gloraSay.Items.Add("");
+                gloraSay.Items.Add("Thanks for asking sir, I am fine, and you?");
+            }
             else if (s.ToLower().Contains("thank you"))
+            {
                 ss.SpeakAsync("pleasure is on my side " + name);
+                gloraSay.Items.Add("");
+                gloraSay.Items.Add("Pleasure is on my side" + name);
+            }
             else if (s.ToLower().Contains("browser") || s.ToLower().Contains("web") || s.ToLower().Contains("google"))
             {
                 ss.SpeakAsync("I am opening google sir!");
                 Process.Start("chrome", "https://www.google.com");
+                gloraSay.Items.Add("");
+                gloraSay.Items.Add("I am opening google sir!");
             }
             else if (s.ToLower().Contains("close"))
             {
@@ -141,6 +188,8 @@ namespace Glora
             {
                 ss.SpeakAsync("Opening YouTube sir");
                 Process.Start("chrome", "https://www.youtube.com");
+                gloraSay.Items.Add("");
+                gloraSay.Items.Add("Opening YouTube sir!");
             }
             else if (s.ToLower().Contains("search "))
             {
@@ -173,7 +222,7 @@ namespace Glora
                 }
 
                 string g = s.Replace(' ', '+');
-                label.Content = g;
+                webPageCode.IsEnabled = true;
                 ss.SpeakAsync("Okay sir, I am searching for it");
                 int y = g.IndexOf("f") + 4;
                 search = g.Substring(y);
@@ -199,23 +248,69 @@ namespace Glora
                     gloraSay.Items.Add("General Kenobi!");
                 }
                 else if (tbCommandForPeople.Text.ToLower().Contains("hello") || tbCommandForPeople.Text.ToLower().Contains("hi"))
+                {
                     ss.SpeakAsync("hello sir");
+                    gloraSay.Items.Add("");
+                    gloraSay.Items.Add("Hello sir!");
+                }
                 else if (tbCommandForPeople.Text.ToLower().Contains("time"))
+                {
                     ss.SpeakAsync("current date is " + DateTime.Now.ToLocalTime());
+                    gloraSay.Items.Add("");
+                    gloraSay.Items.Add(DateTime.Now.ToLocalTime().ToString());
+                }
                 else if (tbCommandForPeople.Text.ToLower().Contains("current date"))
+                {
                     ss.SpeakAsync("current date is " + DateTime.Now.ToLongDateString());
+                    gloraSay.Items.Add("");
+                    gloraSay.Items.Add(DateTime.Now.ToLongDateString());
+                }
                 else if (tbCommandForPeople.Text.ToLower().Contains("are you here"))
+                {
                     ss.SpeakAsync("I am here sir, dont be afraid");
+                    gloraSay.Items.Add("");
+                    gloraSay.Items.Add("I am here sir, don't be afraid!");
+                }
+                else if (tbCommandForPeople.Text.ToLower().Contains("motivate me"))
+                {
+                    ss.SpeakAsync("Lets do it!");
+                    Random random = new Random();
+                    int y = random.Next(0, 7);
+                    ss.SpeakAsync(motivate[y]);
+                    gloraSay.Items.Add("");
+                    gloraSay.Items.Add("Let's do it!");
+                }
                 else if (tbCommandForPeople.Text.ToLower().Contains("fine"))
+                {
                     ss.SpeakAsync("I am glad.");
+                    gloraSay.Items.Add("");
+                    gloraSay.Items.Add("I am glad!");
+                }
+                else if (tbCommandForPeople.Text.ToLower().Contains("10 to 0"))
+                {
+                    ss.SpeakAsync("Ok sir!");
+                    ss.SpeakAsync("10!"); ss.SpeakAsync("9!"); ss.SpeakAsync("8!"); ss.SpeakAsync("7!"); ss.SpeakAsync("6!"); ss.SpeakAsync("5!"); ss.SpeakAsync("4!"); ss.SpeakAsync("3!"); ss.SpeakAsync("2!"); ss.SpeakAsync("1!"); ss.SpeakAsync("Misík!");
+                    gloraSay.Items.Add("");
+                    gloraSay.Items.Add("10,9,8....");
+                }
                 else if (tbCommandForPeople.Text.ToLower().Contains("how are you"))
+                {
                     ss.SpeakAsync("Thanks for asking sir, i am fine, and you?");
+                    gloraSay.Items.Add("");
+                    gloraSay.Items.Add("Thanks for asking, I am fine, and you?");
+                }
                 else if (tbCommandForPeople.Text.ToLower().Contains("thank you"))
+                {
                     ss.SpeakAsync("pleasure is on my side " + name);
+                    gloraSay.Items.Add("");
+                    gloraSay.Items.Add("Pleasure is on my side " + name);
+                }
                 else if (tbCommandForPeople.Text.ToLower().Contains("browser") || tbCommandForPeople.Text.ToLower().Contains("web") || tbCommandForPeople.Text.ToLower().Contains("google"))
                 {
                     ss.SpeakAsync("I am opening google sir!");
                     Process.Start("chrome", "https://www.google.com");
+                    gloraSay.Items.Add("");
+                    gloraSay.Items.Add("I am opening google sir!");
                 }
                 else if (tbCommandForPeople.Text.ToLower().Contains("close"))
                 {
@@ -259,16 +354,18 @@ namespace Glora
                     }
 
                     Process.Start("chrome", "https://www." + tbCommandForPeople.Text);
+                    webPageCode.IsEnabled = true;
                 }
                 else if (tbCommandForPeople.Text.ToLower().Contains("youtube"))
                 {
                     ss.SpeakAsync("Opening YouTube sir");
                     Process.Start("chrome", "https://www.youtube.com");
+                    gloraSay.Items.Add("");
+                    gloraSay.Items.Add("Opening YouTube sir!");
                 }
                 else if (tbCommandForPeople.Text.ToLower().Contains("search "))
                 {
                     string g = tbCommandForPeople.Text.Replace(' ', '+');
-                    label.Content = g;
                     ss.SpeakAsync("Okay sir, I am searching for it");
                     int y = g.IndexOf("f") + 4;
                     search = g.Substring(y);
@@ -300,6 +397,7 @@ namespace Glora
                         response.Close();
                         readStream.Close();
                     }
+                    webPageCode.IsEnabled = true;
 
                     Process.Start("chrome", "https://www.google.com/search?q=" + search);
                 }
@@ -320,6 +418,11 @@ namespace Glora
             {
                 dictateBtn_Click(sender, e);
             }
+        }
+
+        private void webPageCode_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("web.txt");
         }
     }
 }
